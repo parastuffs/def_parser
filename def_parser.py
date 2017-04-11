3870daa97f0a2c741d9caf47650e9f0046b7defc
Quentin Delhaye <parastuffs@gmail.com>
2017-04-11 15:48:45 +0200

    extractPins()
    At first, I did not want to implement the pin extraction.
    But when I began to work on the net extraction, some pin-to-gate nets made it clear that the pin cannot be left aside.
    Indeed, those nets still need to be routed and can be important in the partitioning.
    
    However, I may need to keep to pins pinned in place and not allow the partitioner to move them around. Not sure yet.

--- def_parser.py
+++ def_parser.py
@@ -35,50 +35,50 @@ class Design:
 
         with open("ldpc_5.8.def", 'r') as f:
             for line in f:
-            	# TODO: clean to remove the use of break
-            	# TODO: try to need less try/except
+                # TODO: clean to remove the use of break
+                # TODO: try to need less try/except
                 if endOfComponents:
                     break # Dirty, but I don't care
                 if inComponents:
                     # Parse the line and extract the cell
                     split = line.split(' ')
-                    print split
+                    #print split
                     try:
-                    	split.index(";\n")
+                        split.index(";\n")
                     except:
-	                    gate = Gate(split[1])
-	                    """
-	                    A cell is always defined on a single line.
-	                    On this line, its coordinates are written as
-	                    'PLACED ( <abscissa> <ordinate> )'
-	                    Hence, we simply need to find the 'PLACE' keyword
-	                    and take take the second and third token to extract
-	                    the coordinates.
-	                    """
-	                    # TODO: Is there a way to search the index for two words?
-	                    # Like index("PLACED" | "FIXED')
-	                    try:
-	                    	gate.setX(int(split[split.index("PLACED") + 2]))
-	                    except:
-	                    	try:
-	                    		gate.setX(int(split[split.index("FIXED") + 2]))
-	                    	except:
-	                    		# If this raises an exception, it probably means
-	                    		# we reached the 'END COMPONENTS'
-	                    		pass
+                        gate = Gate(split[1], False)
+                        """
+                        A cell is always defined on a single line.
+                        On this line, its coordinates are written as
+                        'PLACED ( <abscissa> <ordinate> )'
+                        Hence, we simply need to find the 'PLACE' keyword
+                        and take take the second and third token to extract
+                        the coordinates.
+                        """
+                        # TODO: Is there a way to search the index for two words?
+                        # Like index("PLACED" | "FIXED')
+                        try:
+                            gate.setX(int(split[split.index("PLACED") + 2]))
+                        except:
+                            try:
+                                gate.setX(int(split[split.index("FIXED") + 2]))
+                            except:
+                                # If this raises an exception, it probably means
+                                # we reached the 'END COMPONENTS'
+                                pass
 
-	                    try:
-	                    	gate.setY(int(split[split.index("PLACED") + 3]))
-	                    except:
-	                    	try:
-	                    		gate.setY(int(split[split.index("FIXED") + 3]))
-	                    	except:
-	                    		pass
-	                    	else:
-								design.addGate(gate)
-	                    else:
-							design.addGate(gate)
-	                    # endOfComponents = True
+                        try:
+                            gate.setY(int(split[split.index("PLACED") + 3]))
+                        except:
+                            try:
+                                gate.setY(int(split[split.index("FIXED") + 3]))
+                            except:
+                                pass
+                            else:
+                                design.addGate(gate)
+                        else:
+                            design.addGate(gate)
+                        # endOfComponents = True
 
 
                 if 'END COMPONENTS' in line:
@@ -87,6 +87,81 @@ class Design:
 
                 elif 'COMPONENTS' in line:
                     inComponents = True
+
+    def extractPins(self):
+        print "Reading the def to extract pins."
+
+        inPins = False
+        endOfPins = False
+
+        with open("ldpc_5.8.def", 'r') as f:
+            line = f.readline()
+
+            while line:
+
+                if 'PINS ' in line:
+                    inPins = True
+
+                elif 'END PINS' in line:
+                    break
+
+                if inPins and '- ' in line:
+                    # Create the pin gate with its name
+                    gate = Gate(line.split(' ')[1], True)
+                    nextLine = f.readline()
+
+                    # Skip everything up to the 'PLACED' keyword
+                    while not ' PLACED ' in nextLine:
+                        nextLine = f.readline()
+
+                    # Now we are at the 'PLACED' line
+                    gate.setX(int(nextLine.split(' ')[nextLine.split(' ').index("PLACED") + 2]))
+                    gate.setY(int(nextLine.split(' ')[nextLine.split(' ').index("PLACED") + 3]))
+
+                    design.addGate(gate)
+
+                line = f.readline()
+
+    def extractNets(self):
+        print "Reading the def to extract nets."
+
+        endOfNet = False # end of single net
+        endOfNets = False # end of bloc with all the nets
+        inNets = True
+
+        with open("ldpc_5.8.def", 'r') as f:
+            line = f.readline()
+            while line:
+                if ('NETS' in line and not 'SPECIALNETS' in line) or (inNets):
+                    # print line
+                    line = line.strip("\n")
+
+                    if '- ' in line:
+                        # new net
+                        net = Net(line.split(' ')[1])
+                        # print line.split(' ')[1]
+                        # Read the next line after the net name,
+                        # it should contain the connected cells names.
+                        nextLine = f.readline()
+                        while not ' ROUTED ' in nextLine:
+                            split = nextLine.split(' ')
+                            nextLine = f.readline()
+
+
+
+                    if ';' in line:
+                        endOfNet = True
+
+
+
+                    inNets = True
+
+                if 'END NETS' in line:
+                    endOfNets = True
+                    break
+
+                line = f.readline()
+
 
 
     def setWidth(self, width):
@@ -100,16 +175,17 @@ class Design:
         self.gates.append(gate) # Append Gate object
 
 class Net:
-    def __init__(self, name, netID):
+    def __init__(self, name):
         self.name = name
-        self.ID = netID
+        self.ID = 0
         self.wl = 0
 
 class Gate:
-    def __init__(self, name):
+    def __init__(self, name, isPin):
         self.name = name
         self.x = 0
         self.y = 0
+        self.isPin = isPin
 
     def setX(self, x):
         self.x = x
@@ -118,7 +194,7 @@ class Gate:
         self.y = y
 
     def digest(self):
-    	print "Gate " + self.name + " at (" + str(self.x) + ", " + str(self.y) + ")"
+        print "Gate " + self.name + " at (" + str(self.x) + ", " + str(self.y) + ")"
 
 
 if __name__ == "__main__":
@@ -127,3 +203,7 @@ if __name__ == "__main__":
     design.ReadArea()
     design.ExtractCells()
     design.Digest()
+    design.extractPins()
+    design.Digest()
+
+    # design.extractNets()
