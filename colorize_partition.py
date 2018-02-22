ad2dd4b7d56ed6e5c0ad98c4f83494d523763453
Quentin Delhaye <parastuffs@gmail.com>
2018-02-22 15:31:47 +0100

    Clean up

--- colorize_partition.py
+++ colorize_partition.py
@@ -14,22 +14,26 @@ Partition 1: (0, 0, 255)
 if __name__ == "__main__":
 
     def_parser.MEMORY_MACROS = False # spc: True, others: False
+    # def_parser.MEMORY_MACROS = True # spc: True, others: False
     def_parser.UNITS_DISTANCE_MICRONS = 10000 #flipr, boomcore, ldpc
     # def_parser.UNITS_DISTANCE_MICRONS = 1000  #ccx, spc
 
     # ldpc, boomcore, flipr: "7nm"
     # ccx, spc: "45nm"
     def_parser.extractStdCells("7nm")
+    # def_parser.extractStdCells("45nm")
     if def_parser.MEMORY_MACROS:
         def_parser.extractMemoryMacros(14,4)
 
     # If you change the deffile, also change the leffile, MEMORY_MACROS and UNITS_DISTANCE_MICRONS
     # def_parser.deffile = "7nm_Jul2017/ldpc.def"
     # def_parser.deffile = "ldpc_4x4/ldpc-4x4.def"
-    def_parser.deffile = "7nm_Jul2017/BoomCore.def"
+    # def_parser.deffile = "7nm_Jul2017/BoomCore.def"
     # def_parser.deffile = "7nm_Jul2017/flipr.def"
     # def_parser.deffile = "7nm_Jul2017/ccx.def"
     # def_parser.deffile = "7nm_Jul2017/SPC/spc.def" # Don't forget to turn the MEMORY_MACROS on.
+    def_parser.deffile = "ldpc_4x4_serial.def/ldpc-4x4-serial.def"
+    # def_parser.deffile = "ldpc_4x4/ldpc-4x4.def"
 
 
     """
@@ -40,7 +44,7 @@ if __name__ == "__main__":
     """
     # partitionFile = "/home/para/dev/metis_unicorn/temp_design/metis_02_1-NoWires_area.hgr.part"
     # partitionFile = "/home/para/dev/def_parser/2018-01-24_19-37-08/BoomCore_random_9/partitions_2018-02-20_13-03-40/metis_02_1-NoWires_area.hgr.part"
-    mainDir = "/home/para/dev/def_parser/2018-01-24_19-37-08/"
+    mainDir = "/home/para/dev/def_parser/2018-02-21_09-53-50"
 
     def_parser.design = def_parser.Design()
     def_parser.design.ReadArea()
@@ -70,28 +74,13 @@ if __name__ == "__main__":
                                     gate = line.split()[0]
                                     x = def_parser.design.gates.get(gate).x
                                     y = def_parser.design.gates.get(gate).y
-                                    # print x
-                                    # print y
                                     index = (int(y*10) * (imgW) ) + int(x*10)
                                     data[index] = partColors[int(line.split()[1])]
-
-
-                            # for k in clusters:
-                            #     r = int(random.uniform(0, 255))
-                            #     g = int(random.uniform(0, 255))
-                            #     b = int(random.uniform(0, 255))
-                            #     for gate in clusters[k]:
-                            #         x = def_parser.design.gates.get(gate).x
-                            #         y = def_parser.design.gates.get(gate).y
-                            #         # print x
-                            #         # print y
-                            #         index = (int(y*10) * (imgW) ) + int(x*10)
-                            #         data[index] = (r, g, b)
 
                             print "Create the image (" + str(imgW) + ", " + str(imgH) + ")"
                             img = Image.new('RGB', (imgW, imgH))
                             print "Put data"
                             img.putdata(data)
-                            print "save image"
-                            outfile = os.path.join(partitionDir, clusterDir.split('/')[-1] + ".png")
+                            outfile = os.path.join(partitionDir, clusterDir.split('/')[-1] + "_" + partitionFile.split('.')[0] + ".png")
+                            print "save image " + outfile
                             img.save(outfile)
