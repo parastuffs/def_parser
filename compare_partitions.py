0753c3837291b7b5ac393903ab2e5f0b87966ef9
Quentin Delhaye <parastuffs@gmail.com>
2018-02-23 16:34:53 +0100

    Use sets instead of dictionary.

--- compare_partitions.py
+++ compare_partitions.py
@@ -4,7 +4,7 @@ import Image
 import random
 from natsort import natsorted # https://pypi.python.org/pypi/natsort
 import os
-
+from sets import Set
 
 
 if __name__ == "__main__":
@@ -14,17 +14,19 @@ if __name__ == "__main__":
     |_ Cluster level x
         |_ partition
     """
-    mainDir = "/home/para/dev/def_parser/2018-01-24_22-03-05"
+    mainDir = "/home/para/dev/def_parser/2018-02-21_10-11-08"
 
-    # Array of dictionaries, each containing {str(gate name): int(die)}
-    partitions = []
     clusterLevels = []
+
+    # Array of arrays of sets: [[Set_partition0, Set_partition1], ... ]
+    # There is on array of sets per clustering level.
+    partitionSets = []
 
     # Partitions extraction
     for clusterDir in natsorted(os.listdir(mainDir)):
-        clusterLevels.append(clusterDir.split('_')[-1])
         clusterDir = os.path.join(mainDir, clusterDir)
         if os.path.isdir(clusterDir):
+            clusterLevels.append(clusterDir.split('_')[-1])
             for partitionDir in natsorted(os.listdir(clusterDir)):
                 partitionDir = os.path.join(clusterDir, partitionDir)
                 if os.path.isdir(partitionDir):
@@ -32,12 +34,20 @@ if __name__ == "__main__":
                         # I just want to compare 'metis_01'.
                         if "metis_01" in partitionFile and partitionFile.split('.')[-1] == "part":
 
-                            partition = dict()
+                            set0 = Set()
+                            set1 = Set()
 
                             with open(os.path.join(partitionDir, partitionFile), 'r') as f:
                                 for line in f.readlines():
-                                    partition[line.split()[0]] = int(line.split()[1])
-                            partitions.append(partition)
+                                    gate = line.split()[0]
+                                    die = int(line.split()[1])
+
+                                    if die == 0:
+                                        set0.add(gate)
+                                    elif die == 1:
+                                        set1.add(gate)
+
+                            partitionSets.append([set0, set1])
 
     """
     Formating the output file:
@@ -50,7 +60,8 @@ if __name__ == "__main__":
     <cluster level n-1>, , ..., <common gates with n>
     """
     output = ""
-    output += "Total gates, " + str(len(partitions[0])) + "\n"
+    totGates = len(partitionSets[0][0]) + len(partitionSets[0][1])
+    output += "Total gates, " + str(totGates) + "\n"
 
     for i in range(1,len(clusterLevels)):
         output += "," + str(clusterLevels[i])
@@ -58,18 +69,23 @@ if __name__ == "__main__":
 
     outputRelative = output
 
-    for i in range(len(partitions)):
+    for i in range(len(partitionSets)-1):
         output += str(clusterLevels[i])
         output += "," * i
         outputRelative += str(clusterLevels[i])
         outputRelative += "," * i
-        for j in range(i+1, len(partitions)):
-            commonGates = 0
-            for k in partition:
-                if partitions[j].get(k) == partitions[i][k]:
-                    commonGates += 1
+        for j in range(i+1, len(partitionSets)):
+            commonGates = len(partitionSets[i][0].intersection(partitionSets[j][0])) + len(partitionSets[i][1].intersection(partitionSets[j][1]))
             output += "," + str(commonGates)
-            outputRelative += "," + str(100*commonGates/len(partitions[0]))
+
+            # If we have less than half of the gates in the same partition,
+            # we can simply invert the result as it corresponds to inverting
+            # the two dies.
+            # The most similar partitions are at 100%,
+            # the least similar at 50%
+            if commonGates < 0.5*totGates:
+                commonGates = totGates - commonGates
+            outputRelative += "," + str(100*commonGates/totGates)
         output += "\n"
         outputRelative += "\n"
 
