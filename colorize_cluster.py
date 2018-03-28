05bf2e7e4e3efb2a72a44d2f400ee720dfd04597
Quentin Delhaye <parastuffs@gmail.com>
2018-03-28 10:42:41 +0200

    Colorize clusters script.
    Reads the ClustersInstances.out file and with the help of the DEF file, colorizes the clusters.

---
+++ colorize_cluster.py
@@ -0,0 +1,115 @@
+import def_parser
+import Image
+import random
+import os
+
+colors = [  [53, 235, 30],
+            [30, 230, 235],
+            [30, 82, 235],
+            [235, 30, 223],
+            [235, 30, 43],
+            [235, 148, 30],
+            [227, 235, 30],
+            [135, 135, 135]
+        ]
+
+
+
+
+if __name__ == "__main__":
+
+
+    clusterInsFile = "/home/para/dev/def_parser/2018-03-15_14-59-54/flipr_progressive_8/ClustersInstances.out"
+    tech = ""
+
+    if "ldpc-4x4-serial" in clusterInsFile:
+        def_parser.deffile = "ldpc_4x4_serial.def/ldpc-4x4-serial.def"
+        tech = "7nm"
+        def_parser.MEMORY_MACROS = False
+        def_parser.UNITS_DISTANCE_MICRONS = 10000
+    elif "ldpc-4x4" in clusterInsFile:
+        def_parser.deffile = "ldpc_4x4/ldpc-4x4.def"
+        tech = "7nm"
+        def_parser.MEMORY_MACROS = False
+        def_parser.UNITS_DISTANCE_MICRONS = 10000
+    elif "ldpc" in clusterInsFile:
+        def_parser.deffile = "7nm_Jul2017/ldpc.def"
+        tech = "7nm"
+        def_parser.MEMORY_MACROS = False
+        def_parser.UNITS_DISTANCE_MICRONS = 10000
+    elif "BoomCore" in clusterInsFile:
+        def_parser.deffile = "7nm_Jul2017/BoomCore.def"
+        tech = "7nm"
+        def_parser.MEMORY_MACROS = False
+        def_parser.UNITS_DISTANCE_MICRONS = 10000
+    elif "flipr" in clusterInsFile:
+        def_parser.deffile = "7nm_Jul2017/flipr.def"
+        tech = "7nm"
+        def_parser.MEMORY_MACROS = False
+        def_parser.UNITS_DISTANCE_MICRONS = 10000
+    elif "ccx" in clusterInsFile:
+        def_parser.deffile = "7nm_Jul2017/ccx.def"
+        tech = "45nm"
+        def_parser.MEMORY_MACROS = False
+        def_parser.UNITS_DISTANCE_MICRONS = 1000
+    elif "spc" in clusterInsFile:
+        def_parser.deffile = "7nm_Jul2017/SPC/spc.def"
+        tech = "45nm"
+        def_parser.MEMORY_MACROS = True
+        def_parser.UNITS_DISTANCE_MICRONS = 1000
+
+
+    def_parser.extractStdCells(tech)
+    if def_parser.MEMORY_MACROS:
+        def_parser.extractMemoryMacros(14,4)
+
+    # If you change the deffile, also change the leffile, MEMORY_MACROS and UNITS_DISTANCE_MICRONS
+    # TODO make this into cli parameter
+    
+    # d
+    # 
+    # def_parser.deffile = "7nm_Jul2017/ccx.def"
+    # def_parser.deffile = "7nm_Jul2017/SPC/spc.def" # Don't forget to turn the MEMORY_MACROS on.
+
+    def_parser.design = def_parser.Design()
+    def_parser.design.ReadArea()
+    def_parser.design.ExtractCells()
+
+    imgW = int(def_parser.design.width*10) # Width of the design in 10^-1 um
+    imgH = int(def_parser.design.height*10)
+    imgSize = (imgW) * (imgH)
+    data = [0] * imgSize
+
+    clusters = dict()
+
+    with open(clusterInsFile, 'r') as f:
+        for line in f.readlines():
+            clusters[line.split()[0]] = line.split()[1:]
+
+    for i, k in enumerate(clusters):
+        if i < len(colors):
+            r = colors[i][0]
+            g = colors[i][1]
+            b = colors[i][2]
+        else:
+            r = int(random.uniform(0, 255))
+            g = int(random.uniform(0, 255))
+            b = int(random.uniform(0, 255))
+        for gate in clusters[k]:
+            x = def_parser.design.gates.get(gate).x
+            y = def_parser.design.gates.get(gate).y
+            # print x
+            # print y
+            index = (int(y*10) * (imgW) ) + int(x*10)
+            data[index] = (r, g, b)
+
+    print "Create the image (" + str(imgW) + ", " + str(imgH) + ")"
+    img = Image.new('RGB', (imgW, imgH))
+    print "Put data"
+    img.putdata(data)
+    filename = os.path.join(os.sep.join(clusterInsFile.split(os.sep)[:-1]),clusterInsFile.split(os.sep)[-2]) + "_cluster_colors.png"
+    print("save image as ", filename)
+
+    img.save(filename)
+    # print "show image"
+    # img.show()
