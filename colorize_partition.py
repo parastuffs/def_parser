bbb5e996cecd93e573e9f5e4e06dce2ceb935ee6
Quentin Delhaye <parastuffs@gmail.com>
2018-04-12 14:50:29 +0200

    Colorize partitions: lift the Metis restriction.

--- colorize_partition.py
+++ colorize_partition.py
@@ -26,13 +26,13 @@ if __name__ == "__main__":
         def_parser.extractMemoryMacros(14,4)
 
     # If you change the deffile, also change the leffile, MEMORY_MACROS and UNITS_DISTANCE_MICRONS
-    # def_parser.deffile = "7nm_Jul2017/ldpc.def"
+    def_parser.deffile = "7nm_Jul2017/ldpc.def"
     # def_parser.deffile = "ldpc_4x4/ldpc-4x4.def"
     # def_parser.deffile = "7nm_Jul2017/BoomCore.def"
     # def_parser.deffile = "7nm_Jul2017/flipr.def"
     # def_parser.deffile = "7nm_Jul2017/ccx.def"
     # def_parser.deffile = "7nm_Jul2017/SPC/spc.def" # Don't forget to turn the MEMORY_MACROS on.
-    def_parser.deffile = "ldpc_4x4_serial.def/ldpc-4x4-serial.def"
+    # def_parser.deffile = "ldpc_4x4_serial.def/ldpc-4x4-serial.def"
     # def_parser.deffile = "ldpc_4x4/ldpc-4x4.def"
 
 
@@ -44,7 +44,8 @@ if __name__ == "__main__":
     """
     # partitionFile = "/home/para/dev/metis_unicorn/temp_design/metis_02_1-NoWires_area.hgr.part"
     # partitionFile = "/home/para/dev/def_parser/2018-01-24_19-37-08/BoomCore_random_9/partitions_2018-02-20_13-03-40/metis_02_1-NoWires_area.hgr.part"
-    mainDir = "/home/para/dev/def_parser/2018-02-21_09-53-50"
+    # mainDir = "/home/para/dev/def_parser/2018-03-14_19-51-44"
+    mainDir = "//home/para/dev/def_parser/2018-03-13_10-36-02"
 
     def_parser.design = def_parser.Design()
     def_parser.design.ReadArea()
@@ -64,7 +65,28 @@ if __name__ == "__main__":
                     for partitionFile in natsorted(os.listdir(partitionDir)):
                         # I just want to generate the image for the 'metis_01' file.
                         # CF 2017-02-21, MPD: "Comme disait mon pere, c'est des moules dans du sirop.", en parlant du projet batiment.
-                        if "metis_01" in partitionFile and partitionFile.split('.')[-1] == "part":
+                        if "01" in partitionFile and partitionFile.split('.')[-1] == "part":
+                        # if "metis_01" in partitionFile and partitionFile.split('.')[-1] == "part":
+
+                            clusters = dict()
+                            partColors = [(255, 0, 0), (0, 0, 255)]
+
+                            with open(os.path.join(partitionDir, partitionFile), 'r') as f:
+                                for line in f.readlines():
+                                    gate = line.split()[0]
+                                    x = def_parser.design.gates.get(gate).x
+                                    y = def_parser.design.gates.get(gate).y
+                                    index = (int(y*10) * (imgW) ) + int(x*10)
+                                    data[index] = partColors[int(line.split()[1])]
+
+                            print "Create the image (" + str(imgW) + ", " + str(imgH) + ")"
+                            img = Image.new('RGB', (imgW, imgH))
+                            print "Put data"
+                            img.putdata(data)
+                            outfile = os.path.join(partitionDir, clusterDir.split('/')[-1] + "_" + partitionFile.split('.')[0] + ".png")
+                            print "save image " + outfile
+                            img.save(outfile)
+                        elif "03" in partitionFile and partitionFile.split('.')[-1] == "part":
 
                             clusters = dict()
                             partColors = [(255, 0, 0), (0, 0, 255)]
