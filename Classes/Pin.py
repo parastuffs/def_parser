848a4b1b4b20c1cd8245ec774654a8ad85f9e4c3
Quentin <parastuffs@gmail.com>
2019-11-08 12:38:12 +0200

    Missing Pin class.

---
+++ Classes/Pin.py
@@ -0,0 +1,14 @@
+class Pin:
+    def __init__(self, name):
+        self.name = name
+        self.x = 0
+        self.y = 0
+
+    def setX(self, x):
+        self.x = x
+
+    def setY(self, y):
+        self.y = y
+
+    def digest(self):
+        logger.info("Pin {} at ({}, {})".format(self.name, self.x, self.y))
